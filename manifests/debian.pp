f98534fbe5e8dc42ab3a5c9df004cfd356b21e5b
Walter Heck <walter@tribily.com>
2012-02-04 04:42:35 +0200

    Remove hyphen in classnames, not officially supported

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -2,26 +2,26 @@ class augeas::debian inherits augeas::base {
 
   # the version in Debian Lenny is 0.2.2, which is too old and buggy
   if $::lsbdistcodename == 'lenny' {
-    include apt::repo::lenny-backports
+    include apt::repo::lennybackports
   }
-  
+
   package { "augeas-lenses":
     ensure => $::augeas_version,
     before => File["/usr/share/augeas/lenses/contrib"],
   }
-      
+
   package { "libaugeas0":
     ensure => $::augeas_version,
     before => File["/usr/share/augeas/lenses/contrib"],
   }
-  
+
   package { "augeas-tools":
     ensure => $::augeas_version,
     before => File["/usr/share/augeas/lenses/contrib"],
   }
-  
-  package { "libaugeas-ruby1.8": 
-    ensure => present 
+
+  package { "libaugeas-ruby1.8":
+    ensure => present
   }
 
 }
